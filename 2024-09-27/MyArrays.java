340f54b05de8a490a48c9746bc74f2e2775d4028
lesimongus <sliu60@stuy.edu>
2024-09-27 11:37:51 -0400

    set up the arrayToString function and made a test case

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,3 +1,15 @@
 public class MyArrays{
-  
+  public static void main(String[] args) {
+    System.out.println(arrayToString([1, 2, 3, 4]));
+  }
+  public static String arrayToString(int[] nums){
+    String arrString = "[";
+    for (int i = 0; i < nums.length; i++){
+      arrString+= nums[i];
+      if (i < nums.length-1){
+        arrString+= ", ";
+      }
+    }
+    return arrString + "]";
+  }
 }
