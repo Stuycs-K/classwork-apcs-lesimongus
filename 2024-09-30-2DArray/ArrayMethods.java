c3bec708407adb929558685268ac54e82ddaafcb
Simon Liu <sliu60@stuy.edu>
2024-10-01 00:09:39 -0400

    finished up swapRC

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -2,17 +2,24 @@ public class ArrayMethods{
   //Simon Liu: simonl105@nycstudents.net
   //Adi Murgescu: adrianm199@nycstudents.net
   public static void main(String[] args){
-    int[] singtest = {1, 2, 3};
     int[][] test = {{}, {}};
     int[][] test1 = {{1,2,3}, {23, 4, 7}, {16, 15, 3}};
     int[][] test2 = {{1, 2}, {2, 7, 5, 6}, {}};
-    System.out.println(arrToString(singtest));
+    int[][] test3 = {{1, 2, 3}, {4, 5, 6}};
+    int[][] test4 = {{3, 5, 8}, {5, 9, 12}, {11, 15, 19}, {41, 2, 13}};
     System.out.println(arrToString(test));
     System.out.println(arrToString(test1));
     System.out.println(arrToString(test2));
+    System.out.println(arrToString(test3));
+    System.out.println(arrToString(test4));
     System.out.println(arr2DSum(test));
     System.out.println(arr2DSum(test1));
     System.out.println(arr2DSum(test2));
+    System.out.println(arr2DSum(test3));
+    System.out.println(arr2DSum(test4));
+    System.out.println(arrToString(swapRC(test1)));
+    System.out.println(arrToString(swapRC(test3)));
+    System.out.println(arrToString(swapRC(test4)));
   }
   public static String arrToString(int[] ary){
     String arrString = "[";
@@ -59,7 +66,13 @@ public class ArrayMethods{
     * You may assume the array is rectangular and neither rows nor cols is 0.
     * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
     */
-//  public static int[][] swapRC(int[][]nums){
-//    return new int[1][1];
-//  }
+  public static int[][] swapRC(int[][]nums){
+    int[][] swapped = new int[nums[0].length][nums.length];
+    for (int i = 0; i < nums.length; i++){
+      for (int j = 0; j < nums[i].length; j++){
+        swapped[j][i] = nums[i][j];
+      }
+    }
+    return swapped;
+  }
 }
