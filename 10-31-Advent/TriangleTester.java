864eb1ce935a699cb31c24b720d8698b55b186a4
lesimongus <sliu60@stuy.edu>
2024-10-31 11:57:13 -0400

    started work on part a

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,15 +1,43 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 public class TriangleTester{
+  public static boolean isTriangle(String sides){
+    Scanner input = new Scanner(sides);
+    int[] side = new int[3];
+    int i = 0;
+    while (input.hasNext()){
+      side[i]=input.nextInt();
+      i++;
+    }
+    if (side[0] + side[1] < side[2]){
+      return false;
+    }
+    if (side[1]+side[2] < side[0]){
+      return false;
+    }
+    if (side[0] + side[2] < side[1]){
+      return false;
+    }
+    return true;
+  }
+  public static int countTrianglesA(String filename){
+    try{
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+      int result = 0;
+      while(input.hasNextLine()){
+        if(isTriangle(input.nextLine())){
+          result++;
+        }
+      }
+      return result;
+    } catch(FileNotFoundException ex){
+        System.out.println("file not found");
+        return;
+    }
+  }
   public static void main(String[] args){
-    Scanner sc1 = new Scanner("This is a bunch of words");
-    while (sc1.hasNext()){
-      System.out.println(sc1.next());
-    }
-    Scanner sc2 = new Scanner("10.0 1.04 99.0 -3.0 4.2");
-    double sc2sum =0;
-    while (sc2.hasNextDouble()){
-      sc2sum+= sc2.nextDouble();
-    }
-    System.out.println(sc2sum);
+    System.out.println(countTrianglesA("inputTri.txt"));
   }
 }
