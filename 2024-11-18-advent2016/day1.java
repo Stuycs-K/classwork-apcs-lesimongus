f809ea95fd879d718a93f7bab0a4a828aff3bee0
lesimongus <sliu60@stuy.edu>
2024-11-18 11:44:28 -0500

    tried to work on day1

---
+++ 2024-11-18-advent2016/day1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class day1{
+  public static int distance(String filename){
+    Scanner a = new Scanner(filename);
+    int angle = 0;
+    int xdist = 0;
+    int ydist = 0;
+    while (a.hasNext()){
+
+    }
+  }
+}
