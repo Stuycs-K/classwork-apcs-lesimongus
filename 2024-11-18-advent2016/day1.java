586eb0a7c1fcad1d5e08270e48323eb4867624d5
lesimongus <sliu60@stuy.edu>
2024-11-19 11:34:50 -0500

    made the parse function

--- 2024-11-18-advent2016/day1.java
+++ 2024-11-18-advent2016/day1.java
@@ -1,9 +1,21 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 public class day1{
-  public static int distance(String filename){
+  public static String[] parse(String filename){
+    try{
+      File file = new File(filename);
+      Scanner a = new Scanner(file);
+      String line = a.nextLine();
+      return line.split(", ");
+    }catch(Exception e){
+      System.out.println("no file");
+    }
+    return null;
+  }
+  /* public static int distance(String filename){
     Scanner a = new Scanner(filename);
     int angle = 0;
     int xdist = 0;
@@ -11,5 +23,8 @@ public class day1{
     while (a.hasNext()){
 
     }
+  } */
+  public static void main(String[] args){
+    System.out.println(Arrays.toString(parse("input.txt")));
   }
 }
