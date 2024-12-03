b210b86843a959a0ad853f0ef2a7e8cfa7951a16
lesimongus <sliu60@stuy.edu>
2024-12-03 11:57:26 -0500

    started

---
+++ 2024-12-03-colors/ColorDemo.java
@@ -0,0 +1,10 @@
+public class ColorDemo{
+  public static void main(String[] args){
+    for (int r = 0; r < 256; r++){
+      for (int b = 0; b < 256; b++){
+        System.out.print("\u001b[38;2;"+r+100+b+"mHi ");
+      }
+    }
+    System.out.println();
+  }
+}
