f55b8da291bf0b5c740e62cfb8bbc689a190a134
lesimongus <sliu60@stuy.edu>
2024-12-04 11:19:41 -0500

    started work on driver

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -2,7 +2,7 @@ public class ColorDemo{
   public static void main(String[] args){
     for (int r = 0; r < 256; r++){
       for (int b = 0; b < 256; b++){
-        System.out.print("\u001b[38;2;"+r+100+b+"mHi ");
+        System.out.print("\u001b[38;2;"+r+";100;"+b+";mHi ");
       }
     }
     System.out.println();
