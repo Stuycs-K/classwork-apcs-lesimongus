e00cd727169559f0c1c64ad1e75ec68c7fea2b30
lesimongus <sliu60@stuy.edu>
2024-12-09 11:45:15 -0500

    added userinput

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 public class Game{
   public static void main(String[] args){
-    Adventurer player = new Fighter("you", 10,10, 50);
+    Scanner userInput = new Scanner(System.in);
+    String username = userInput.nextLine();
+    Adventurer player = new Fighter(username, 10,10, 50);
     Adventurer enemy = new CodeWarrior("the enemy");
     while (player.getHP() > 0 && enemy.getHP() > 0){
       System.out.println(player + ", " + player.getHP() + "/" + player.getmaxHP() + "HP, " + player.getSpecial() + "/" + player.getSpecialMax() + " " + player.getSpecialName());
