73b9698d627523bb9b0a8b8a94e1f5d3bb63231c
lesimongus <sliu60@stuy.edu>
2025-01-03 11:43:30 -0500

    added makewords

--- 2025-01-02/fibonacci.java
+++ 2025-01-02/fibonacci.java
@@ -7,7 +7,19 @@ public class fibonacci{
       return n;
     }
   }
+  public static void makeWords(int remainingLetters, String result, String alphabet){
+    if (remainingLetters > 0){
+      for (int i = 0; i < alphabet.length(); i++){
+        makeWords(remainingLetters - 1, result + alphabet.substring(i, i+1), alphabet);
+      }
+    }
+    else{
+      System.out.println(result);
+    }
+  }
   public static void main(String[] args){
     System.out.println(fib(Integer.parseInt(args[0])));
+    makeWords(2, "", "abc");
+    makeWords(3,"","xy");
   }
 }
